bd08713913c29ceb5fc2a1ad754b96c72f4c5302
Programming Hero <programming.hero1@gmail.com>
2023-05-20 22:18:41 -0500

    implemented search

--- src/pages/Home/Services/Services.jsx
+++ src/pages/Home/Services/Services.jsx
@@ -1,16 +1,23 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import ServiceCard from "./ServiceCard";
 
 
 const Services = () => {
     const [services, setServices] = useState([]);
     const [asc, setAsc] = useState(true);
+    const searchRef = useRef(null);
+    const [search, setSearch] = useState('');
 
     useEffect(() => {
-        fetch(`http://localhost:5000/services?sort=${asc ? 'asc' : 'desc' }`)
+        fetch(`http://localhost:5000/services?search=${search}&sort=${asc ? 'asc' : 'desc'}`)
             .then(res => res.json())
             .then(data => setServices(data));
-    }, [asc])
+    }, [asc, search])
+
+    const handleSearch = () => {
+        console.log(searchRef.current.value);
+        setSearch(searchRef.current.value);
+    }
 
     return (
         <div className="mt-4">
@@ -18,6 +25,14 @@ const Services = () => {
                 <h3 className="text-2xl font-bold text-orange-600">Our Services</h3>
                 <h2 className="text-5xl">Our Service Area</h2>
                 <p>the majority have suffered alteration in some form, by injected humour, or randomised <br /> words which do not look even slightly believable. </p>
+                <div className="form-control">
+                    <div className="input-group">
+                        <input type="text" ref={searchRef} placeholder="Search…" className="input input-bordered" />
+                        <button onClick={handleSearch} className="btn btn-square">
+                            <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
+                        </button>
+                    </div>
+                </div>
                 <button
                     className="btn btn-primary"
                     onClick={() => setAsc(!asc)}
